7c07eefa4c3af5a061a4792cadec681c394f21a5
TiagoPintoAlves <stvtiago@gmail.com>
2015-09-15 21:24:42 -0300

    Update Elfo.java

--- src/modulo-01-java-OO/Modulo01/Elfo.java
+++ src/modulo-01-java-OO/Modulo01/Elfo.java
@@ -1,9 +1,19 @@
 public class Elfo {
     private String nome;
-    private int flechas;
+    private int flechas = 42;
+    private double experiencia = 0.0;
     
     public Elfo(String n, int flech) {
         nome = n;
         flechas = flech;
+    }
+    
+    public atirarFlecha(this.flechas, this.experiencia){
+        if(boolean acertar == true){
+            flechas--;
+            experiencia++;
+        }else{
+            flechas--;
+        }
     }
 }
