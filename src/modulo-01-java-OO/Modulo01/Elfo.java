1cc76602d7ae2388edec162236f894a1e05d94a3
TiagoPintoAlves <stvtiago@gmail.com>
2015-09-16 21:34:01 -0300

    Update Elfo.java

--- src/modulo-01-java-OO/Modulo01/Elfo.java
+++ src/modulo-01-java-OO/Modulo01/Elfo.java
@@ -4,21 +4,49 @@ public class Elfo {
     private int flechas = 42;
     private int experiencia = 0;
     
-    public Elfo(String n, int arrows) {   // Constroi Elfo conforme informaçoes e testa o numero de flechas.
-        if(arrows > 0){
-            GeradorElfo(n, arrows);       // Flechas = nao default.
-        }else{
-            GeradorElfo(n, this.flechas); // Flechas = default.
+        /* Type initializer
+     * Executa antes de cada construtor
+    {
+        flechas = 42;
+    }
+    */
+    
+    public Elfo(String nome, int flechas) {
+        this.nome = nome;
+        this.flechas = flechas;
+    }
+    
+    public Elfo(String nome) {
+        this(nome, 42);
+    }
+    
+    public Elfo(String nome, Integer flechas) {
+        this(nome);
+        if (flechas != null) {
+            this.flechas = flechas;
         }
     }
     
-    public void GeradorElfo(String n, int arrows){ // Método gerador de elfos.
-        nome = n;
-        flechas = arrows;
+    public void atirarFlecha(Dwarf dwarf) {
+        flechas--;
+        experiencia++;
+        dwarf.receberFlechada();
+        //experiencia += 1;
+        //experiencia = experiencia + 1;
     }
     
-    public void atirarFlecha(int arrows){ // Possibilitando a ação do personagem.
-            flechas--; 			// Já descontando as flechas e acrecentando Exp.
-            experiencia++;
+    public String getNome() {
+        return nome;
     }
+    
+    public int getFlechas() {
+        return this.flechas;
+    }
+    
+    /*
+    public void setFlechas(int flechas) {
+        if (flechas > this.flechas)
+            this.flechas = flechas;
+    }
+    */
 }
