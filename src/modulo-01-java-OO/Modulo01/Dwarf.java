02559614c15ae31c8946cc8a1d1bd6dfd9b176a6
TiagoPintoAlves <stvtiago@gmail.com>
2015-09-17 17:41:57 -0300

    Update Dwarf.java

--- src/modulo-01-java-OO/Modulo01/Dwarf.java
+++ src/modulo-01-java-OO/Modulo01/Dwarf.java
@@ -1,11 +1,25 @@
-public class Dwarf {
-    private int vida = 110;
+public class Dwarf{
+    private String nome;
+    private int life;
+    private Status dwarfStatus = Status.VIVO;
     
-    public void receberFlechada() {
-        this.vida -= 10;
+    public Dwarf(String nome){
+       this.nome = nome;
+       this.life = 110;
     }
     
-    public int getVida() {
-        return this.vida;
+    public int getLife(){
+        return this.life;
     }
-} 
+    
+    public void receberFlechada(){
+         this.life -= 10;
+    }
+    
+    public Status getStatus(){
+        if(this.dwarfStatus == dwarfStatus.MORTO){
+            return dwarfStatus.MORTO;
+        }
+        return this.dwarfStatus;
+    }
+}
