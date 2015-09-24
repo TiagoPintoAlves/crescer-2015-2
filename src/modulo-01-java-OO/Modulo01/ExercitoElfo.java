b08e855bced40536cfd4a0574a3437c2a5f1702c
Tiago Alves <stvtiago@gmail.com>
2015-09-24 05:47:18 -0300

    feat(ExercitoElfo): add novos metodos.

--- src/modulo-01-java-OO/Modulo01/ExercitoElfo.java
+++ src/modulo-01-java-OO/Modulo01/ExercitoElfo.java
@@ -1,8 +1,8 @@
 import java.util.*;
 
 public class ExercitoElfo{
-    HashMap<String, Elfo> exercitoElfo;
-    HashMap<Status, ArrayList<Elfo>> statusElfo;
+    Map<String, Elfo> exercitoElfo;
+    Map<Status, ArrayList<Elfo>> statusElfo;
 
     public ExercitoElfo(){
         exercitoElfo = new HashMap<>();
@@ -30,22 +30,43 @@ public class ExercitoElfo{
         statusElfo = new HashMap<>();
         for(String nome : exercitoElfo.keySet()){
             Elfo elfo = exercitoElfo.get(nome);
-            
+
             if(elfo.getStatus() == Status.VIVO){
-                
+
                 if(statusElfo.get(statusVivo) == null){
                     statusElfo.put(statusVivo, new ArrayList<Elfo>());
                 }
                 statusElfo.get(statusVivo).add(elfo);
-                
+
             }else{
-                
+
                 if(statusElfo.get(statusMorto) == null){
                     statusElfo.put(statusMorto, new ArrayList<Elfo>());
                 }
                 statusElfo.get(statusMorto).add(elfo);
             }
         }
+    }
+
+    public ArrayList<Elfo> buscar(Status status){
+        ArrayList<Elfo> elfos = new ArrayList<>();
+        if(status == Status.VIVO){
+            for(Map.Entry<Status, ArrayList<Elfo>> ListaElfos : statusElfo.entrySet()){
+                Status key = ListaElfos.getKey();
+                if(key == Status.VIVO){
+                    elfos = ListaElfos.getValue();
+                }
+            }
+        }else if(status == Status.MORTO){
+            for(Map.Entry<Status, ArrayList<Elfo>> ListaElfos : statusElfo.entrySet()){
+                Status key = ListaElfos.getKey();
+                if(key == Status.MORTO){
+                    elfos = ListaElfos.getValue();
+                }
+            }
+
+        }
+        return elfos;
     }
 
 }
