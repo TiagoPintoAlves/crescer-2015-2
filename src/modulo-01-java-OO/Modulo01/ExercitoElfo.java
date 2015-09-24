4d7b21b7858b260c64522631d4e6fb5e26cc283d
Tiago Alves <stvtiago@gmail.com>
2015-09-24 05:25:23 -0300

    add(ExercitoElfo): agruparStatus;

--- src/modulo-01-java-OO/Modulo01/ExercitoElfo.java
+++ src/modulo-01-java-OO/Modulo01/ExercitoElfo.java
@@ -2,6 +2,7 @@ import java.util.*;
 
 public class ExercitoElfo{
     HashMap<String, Elfo> exercitoElfo;
+    HashMap<Status, ArrayList<Elfo>> statusElfo;
 
     public ExercitoElfo(){
         exercitoElfo = new HashMap<>();
@@ -21,6 +22,30 @@ public class ExercitoElfo{
         }
         return null;
     }
-    
-    
+
+    public void agruparPorStatus(){
+        Status statusVivo = Status.VIVO;
+        Status statusMorto = Status.MORTO;
+
+        statusElfo = new HashMap<>();
+        for(String nome : exercitoElfo.keySet()){
+            Elfo elfo = exercitoElfo.get(nome);
+            
+            if(elfo.getStatus() == Status.VIVO){
+                
+                if(statusElfo.get(statusVivo) == null){
+                    statusElfo.put(statusVivo, new ArrayList<Elfo>());
+                }
+                statusElfo.get(statusVivo).add(elfo);
+                
+            }else{
+                
+                if(statusElfo.get(statusMorto) == null){
+                    statusElfo.put(statusMorto, new ArrayList<Elfo>());
+                }
+                statusElfo.get(statusMorto).add(elfo);
+            }
+        }
+    }
+
 }
