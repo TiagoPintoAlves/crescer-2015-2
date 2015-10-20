f47c0669fdf031efca36326daebfb8af9619993b
Tiago Alves <stvtiago@gmail.com>
2015-10-20 14:23:51 -0200

    feat(colecoes-js): add parameters

--- src/modulo-03-web/dia-02/exercicios-colecoes-js.js
+++ src/modulo-03-web/dia-02/exercicios-colecoes-js.js
@@ -1,9 +1,39 @@
+var clubes = [
+  {
+    nome: 'Arsenal',
+    titulos: [
+      { desc: 'Nacionais', qtd: 13 },
+      { desc: 'Continentais', qtd: 0 },
+      { desc: 'Mundiais', qtd: 0 }
+    ]
+  },
+  {
+    nome: 'Manchester United',
+    titulos: [
+      { desc: 'Nacionais', qtd: 20 },
+      { desc: 'Continentais', qtd: 3 },
+      { desc: 'Mundiais', qtd: 2 }
+    ]
+  },
+  {
+    nome: 'Liverpool',
+    titulos: [
+      { desc: 'Nacionais', qtd: 18 },
+      { desc: 'Continentais', qtd: 5 },
+      { desc: 'Mundiais', qtd: 0 }
+    ]
+  },
+  {
+    nome: 'Chelsea Football Club',
+    titulos: [
+      { desc: 'Nacionais', qtd: 5 },
+      { desc: 'Continentais', qtd: 1 },
+      { desc: 'Mundiais', qtd: 0 }
+    ]
+  }
+];
 
 //Exercicio 1.A
-function byname(time_a, time_b) {
-    return time_a.nome < time_b.nome;
-}
-
 function filterByQtd(Array){
   var maior = 0;
   if(Array.titulos[0].qtd > maior){
