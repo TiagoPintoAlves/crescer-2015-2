fe6c5063cedefef3c0a7b332d2c4b32601bb8e10
Tiago Alves <stvtiago@gmail.com>
2015-11-18 01:43:19 -0200

    feat(ConsoleApp): completar aplicacao

--- src/modulo-05-java/dia-02/JDBC/src/linkedlist/exercicio/Aplicacao.java
+++ src/modulo-05-java/dia-02/JDBC/src/linkedlist/exercicio/Aplicacao.java
@@ -1,5 +1,6 @@
 package linkedlist.exercicio;
 
+import java.awt.List;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
@@ -10,22 +11,29 @@ public class Aplicacao extends Exception {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        File file = new File("C:\\Users\\tiago.alves\\Downloads");
-        LinkedList linkedList = new LinkedList();
+        //File file = new File("C:\\Users\\tiago.alves\\Downloads");
+        File file = new File("C:\\Users\\vicente\\Downloads");
         int opcao = 0;
-        int escolha;
+        int escolhaContinuar;
         boolean continuar;
-
+        int escolhaLista;
+        
+    	System.out.println("Escolha um tipo de lista:");
+    	System.out.println("1- Lista simples\n2- Lista dupla\n");
+    	escolhaLista = scanner.nextInt();
+    	ListAction lista = (escolhaLista == 1)? new LinkedList() : new DoublyLinkedList();
+        
         do{
+        	
             System.out.println("Digite uma opcao:");
             StringBuilder alternativas = new StringBuilder();
             alternativas.append("1- Adicionar na lista\n");
-            alternativas.append("2- Remover da lista\n");
-            alternativas.append("3- Buscar na Lista\n");
+            alternativas.append("2- Adicionar no primeiro da lista\n");
+            alternativas.append("3- Remover da lista\n");
             alternativas.append("4- Listagem completa\n");
             alternativas.append("5- Imprimir Lista\n");
             System.out.println(alternativas);
-
+            
             try{
                 opcao = scanner.nextInt();
             }catch(Exception e){
@@ -34,12 +42,38 @@ public class Aplicacao extends Exception {
 
             switch(opcao){
                 case 1:
+                	try{
+                		System.out.println("Digite o valor a ser inserido:");
+                    	String value = scanner.next();
+                    	System.out.println("Digite a posicao a ser inserido:");
+                    	int index = scanner.nextInt();
+                    	lista.add(index, value);
+                    	System.out.println("Elemento adicionado");
+                	} catch(Exception e){
+                		System.err.println(e);
+                	}
                     break;
                 case 2:
+                	try{
+                		System.out.println("Digite o valor a ser inserido:");
+                    	String value = scanner.next();
+                    	lista.addFirst(value);
+                    	System.out.println("Elemento adicionado");
+                	} catch(Exception e){
+                		System.err.println(e);
+                	}
                     break;
                 case 3:
+                	try{
+                		System.out.println("Digite a posicao do elemento:");
+                		int index = scanner.nextInt();
+                		lista.remove(index);
+                	} catch(Exception e){
+                		System.err.println(e);
+                	}
                     break;
                 case 4:
+                	System.out.println(lista.list());
                     break;
                 case 5:
                     try {
@@ -48,20 +82,19 @@ public class Aplicacao extends Exception {
                         FileWriter fw = new FileWriter(file);
                         BufferedWriter buff = new BufferedWriter(fw);
 
-                        buff.write(linkedList.list().toString());
+                        buff.write(lista.list().toString());
 
                     } catch (IOException e) {
                         System.err.println(e);
                     }
-
                     break;
                 default:
                     System.out.println("Erro. Selecione uma das opcoes listadas.");
             }
-
+            
             System.out.println("Deseja continuar ? 1-Sim ou 2-Nao");
-            escolha = scanner.nextInt();
-            continuar = (escolha == 1)? true : false;
+            escolhaContinuar = scanner.nextInt();
+            continuar = (escolhaContinuar == 1)? true : false;
         }while(continuar == true);
     }
 }
