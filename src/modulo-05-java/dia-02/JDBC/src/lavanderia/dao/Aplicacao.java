0f7994bd70032d694c3904f27f783326ca0b9bac
Tiago Alves <stvtiago@gmail.com>
2015-11-17 23:08:19 -0200

    feat(ServicoDao): implementar insert e select

--- src/modulo-05-java/dia-02/JDBC/src/lavanderia/dao/Aplicacao.java
+++ src/modulo-05-java/dia-02/JDBC/src/lavanderia/dao/Aplicacao.java
@@ -4,24 +4,45 @@ import java.sql.SQLException;
 import java.util.List;
 
 import lavanderia.model.Cliente;
+import lavanderia.model.Servico;
 
 public class Aplicacao {
 
     public static void main(String[] args) throws SQLException {
-        /*
-         * Cliente cliente = new Cliente();
-         * cliente.setIdCliente(1L);
-         * cliente.setNmCliente("Teste");
-         * cliente.setNrCpf("123");
-         * ClienteDao clienteDao = new ClienteDao();
-         * clienteDao.insert(cliente);
-         */
-
+        
+    	//ClienteDao:
         ClienteDao clienteDao = new ClienteDao();
-
-        List<Cliente> lista = clienteDao.listAll();
-        for (Cliente cliente : lista) {
+    	
+        /*
+        Cliente cliente = new Cliente();
+        cliente.setIdCliente(2);
+        cliente.setNmCliente("Outro");
+        cliente.setNrCpf("321");
+        clienteDao.insert(cliente);
+        */
+        
+        //Select Cliente:
+        List<Cliente> listaCliente = clienteDao.listAll();
+        for (Cliente cliente : listaCliente) {
             System.out.println(cliente.getNmCliente());
         }
+        
+        //ServicoDao:
+        //Insert Servico:
+        ServicoDao servicoDao = new ServicoDao();
+        
+        /*
+        Servico servico = new Servico();
+        servico.setIdServico(1);
+        servico.setDsServico("Lavar");
+        servicoDao.insert(servico);
+        */
+        
+        //Select Servico:
+        List<Servico> listaServico = servicoDao.listAll();
+        for(Servico servico : listaServico){
+        	System.out.println(servico.getDsServico());
+        }
+        
     }
 }
